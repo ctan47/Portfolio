d0ad66c32592620d67914c9a319ba7f9fbd5b17b
Chetan Hatkar <44762312+ctan47@users.noreply.github.com>
2023-05-26 22:40:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,16 @@
-// Add a smooth scroll effect to internal links
-$(document).ready(function() {
-  $('a[href^="#"]').on('click', function(event) {
-    var target = $(this.getAttribute('href'));
-    if (target.length) {
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
+// Add your custom JavaScript interactions here
+
+// Show project images on click
+document.addEventListener("DOMContentLoaded", function() {
+  const projectContent = document.querySelectorAll(".content");
+  
+  projectContent.forEach(content => {
+    const image = content.querySelector(".project-image");
+    if (image) {
+      content.addEventListener("click", function() {
+        image.classList.add("show-image", "animate-fade");
+      });
     }
   });
 });
 
-// Show a tooltip when hovering over skills
-$(document).ready(function() {
-  $('[data-toggle="tooltip"]').tooltip();
-});
-
-// Add a "back to top" button that appears when the user scrolls down
-$(document).ready(function() {
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn();
-    } else {
-      $('.back-to-top').fadeOut();
-    }
-  });
-  $('.back-to-top').click(function() {
-    $('html, body').animate({
-      scrollTop: 0
-    }, 1000);
-    return false;
-  });
-});
